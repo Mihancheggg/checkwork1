2f33b7833fc7cee75114e37e018615a4fb5feaae
Mihail Schutow <schutow.mihail@yandex.ru>
2022-07-09 20:17:26 +0300

    Everything except localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
     const increaseFunc = () => {
         if (count < max) {
             setCount(count + 1)
+            setValuesToLocalStorage()
         }
     }
 
@@ -23,33 +24,68 @@ function App() {
         setCount(start)
     }
 
-    const changeStartValue = () => {
-
+    const changeStartValue = (value: number) => {
+        setStartValue(value)
     }
 
-    const changeStartAndMax = () => {
+    const changeMaxValue = (value: number) => {
+        setMaxValue(value)
+    }
+
+    const setStartAndMax = () => {
         setStart(startValue)
         setMax(maxValue)
+        setCount(startValue)
+        setValuesToLocalStorage()
+    }
+
+    const setValuesToLocalStorage = () => {
+        localStorage.setItem('currentValue', JSON.stringify(count))
+        localStorage.setItem('startValue', JSON.stringify(start))
+        localStorage.setItem('maxValue', JSON.stringify(max))
+    }
+
+    const getCurrentFromLocalStorage = () => {
+        let currentFromLocal = localStorage.getItem('currentValue')
+        if (currentFromLocal) {
+            setCount(JSON.parse(currentFromLocal))
+        }
+    }
+
+    const getStartFromLocalStorage = () => {
+        let startFromLocal = localStorage.getItem('startValue')
+        if (startFromLocal) {
+            setStart(JSON.parse(startFromLocal))
+        }
+    }
+
+    const getMaxFromLocalStorage = () => {
+        let maxFromLocal = localStorage.getItem('maxValue')
+        if (maxFromLocal) {
+            setMax(JSON.parse(maxFromLocal))
+        }
     }
 
     return (
         <div className="Parent">
             <div className="App">
                 <div className="container">
-                    <Input title={'Start value: '} value={startValue} onChange={()=>{}}/>
-                    <Input title={'Max value: '} value={maxValue} onChange={()=> {}}/>
+                    <Input title={'Start value: '} value={startValue} onChange={changeStartValue}/>
+                    <Input title={'Max value: '} value={maxValue} onChange={changeMaxValue}/>
                 </div>
-                <Button disabled={count === max} name={'Set'} callback={changeStartAndMax}/>
+                <Button disabled={startValue >= maxValue || startValue < 0 || (startValue === start && maxValue === max)} name={'Set'} callback={setStartAndMax}/>
             </div>
             <div className="App">
-                <Counter max={max} count={count}/>
+                {(startValue < maxValue && startValue >= 0) ? <Counter max={max} count={count}/> :
+                    (startValue < maxValue && startValue < 0) ?
+                        <div className={'likeACounter'}>Start value must be positive or equal to 0!</div> :
+                        <div className={'likeACounter'}>Start value must be less then max value!</div>}
                 <div className="container">
                     <Button disabled={count === max} name={'Increase'} callback={increaseFunc}/>
                     <Button disabled={count === start} name={'Reset'} callback={resetFunc}/>
                 </div>
             </div>
         </div>
-
     );
 }
 
--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -4,15 +4,19 @@ import styles from './Input.module.css'
 export type InputPropsType = {
     title: string
     value: number
-    onChange: () => void
+    onChange: (value: number) => void
 }
 
 export const Input = (props: InputPropsType) => {
 
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        props.onChange(Number(e.currentTarget.value))
+    }
+
     return (
         <div className={styles.inputContainer}>
             <b>{props.title}</b>
-            <input type='number' value={props.value} onChange={()=>{}}/>
+            <input type='number' value={props.value} onChange={onChangeHandler}/>
         </div>
 
     );
